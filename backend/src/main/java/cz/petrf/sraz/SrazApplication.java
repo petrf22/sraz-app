e2a8a8bd15445e0814c9de5df20ba3bff81d6cfd
Petr Franta <petr.franta@gmail.com>
2025-10-28 21:38:46 +0100

    Inicializační commit

---
+++ backend/src/main/java/cz/petrf/sraz/SrazApplication.java
@@ -0,0 +1,13 @@
+package cz.petrf.sraz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrazApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cz/petrf/sraz/SrazApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.petrf.sraz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
