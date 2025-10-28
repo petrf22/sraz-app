b30df1cb6fdfb8d0d1a4426afafc9280c8cb7fd2
Petr Franta <petr.franta@gmail.com>
2025-10-28 21:59:39 +0100

    Pár pokusů - java 25, gradle 9, Angular 20, ng-zorro

---
+++ backend/src/main/java/cz/petrf/sraz/db/entity/User.java
@@ -0,0 +1,45 @@
+// User.java
+package cz.petrf.sraz.db.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String publicName;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private Instant emailVerifiedAt;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
+}
