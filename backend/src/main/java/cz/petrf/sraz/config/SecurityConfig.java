f7c7de64d38e8bf390484a0d98176723cdb41443
Petr Franta <petr.franta@gmail.com>
2025-11-01 00:23:23 +0100

    Nástřel pro GraphQL

--- backend/src/main/java/cz/petrf/sraz/config/SecurityConfig.java
+++ backend/src/main/java/cz/petrf/sraz/config/SecurityConfig.java
@@ -3,11 +3,24 @@ package cz.petrf.sraz.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/graphql", "/graphiql", "/vendor/graphiql/**").permitAll()
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
